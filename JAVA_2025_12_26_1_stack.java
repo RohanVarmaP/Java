95554d0ce412481b55d224e4f1394c206dc9e9b6
rohanvarma-volta <rohanvarma@voltacabs.com>
2025-12-26 15:37:57 +0530

    completed stack

--- JAVA_2025_12_26_1_stack.java
+++ JAVA_2025_12_26_1_stack.java
@@ -1,9 +1,25 @@
 class Stack{
-    private int stack[]=new int[5];
+    private int size=5;
+    private int stack[]=new int[size];
     private int top=0;
 
+    public Stack(int size) {
+        this.size = size;
+    }
+    public Stack() {
+        this.size = 5;
+    }
+
+    public boolean isEmpty(){
+        return top==0?true:false;
+    }
+
+    public boolean isFull(){
+        return top==size?true:false;
+    }
+
     public void push(int num){
-        if(top!=5){
+        if(!isFull()){
             stack[top]=num;
             top++;
         }else{
@@ -11,17 +27,20 @@ class Stack{
         }
     }
 
-    public void pop(){
-        if(top!=0){
-            stack[top]=0;
+    public int pop(){
+        if(!isEmpty()){
+            int num=stack[top-1];
+            stack[top-1]=0;
             top--;
+            return num;
         }else{
             System.out.println("Stack is empty.");
+            return -1;
         }
     }
 
     public int peek(){
-        if(top!=0){
+        if(!isEmpty()){
             return stack[top-1];
         }else{
             System.out.println("Stack is empty.");
@@ -33,19 +52,22 @@ class Stack{
 
 public class JAVA_2025_12_26_1_stack {
     public static void main(String[] args) {
-        Stack s=new Stack();
+        Stack s=new Stack(3);
         s.push(2);
-        s.peek(); 
+        System.out.println("peek: "+s.peek()); 
         s.push(3);
-        s.peek(); 
+        System.out.println("peek: "+s.peek()); 
         s.push(1);
-        s.peek(); 
+        System.out.println("peek: "+s.peek()); 
         s.push(6);
-        s.peek(); 
-        s.pop();
-        s.peek();
-        s.pop();
-        s.peek();
-        s.pop();
-        s.peek();
+        System.out.println("peek: "+s.peek());
+        System.out.println("pop: "+s.pop());
+        System.out.println("peek: "+s.peek());
+        System.out.println("pop: "+s.pop());
+        System.out.println("peek: "+s.peek());
+        System.out.println("pop: "+s.pop());
+        System.out.println("peek: "+s.peek());
+        System.out.println("pop: "+s.pop());
+        System.out.println("peek: "+s.peek());
+    }
 }
